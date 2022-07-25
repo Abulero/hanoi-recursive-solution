d7a2c147221dcfd41c7f9cce102a5879133b344c
AndreCecchi <andre.cecchi@mendix.com>
2022-07-25 17:45:01 +0200

    Simplify code

--- hanoi.py
+++ hanoi.py
@@ -1,13 +1,10 @@
 def hanoi(n, start, support, finish):
     if (n > 1):
         hanoi(n-1, start, finish, support)
-        move(start, finish)
+        print("Move " + start + " to " + finish)
         hanoi(n-1, support, start, finish)
     else:
-        move(start, finish)
-
-def move(origin, destination):
-    print(origin + " -> " + destination)
+        print("Move " + start + " to " + finish)
 
 if __name__ == '__main__':
-    hanoi(3, 'Left', 'Center', 'Right')
+    hanoi(3, 'L', 'C', 'R')
