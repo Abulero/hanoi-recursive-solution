d5b06292b362dfced8ec023fae11432fb39e5d00
AndreCecchi <andre.cecchi@mendix.com>
2022-07-26 15:39:37 +0200

    Change order of conditions

--- hanoi.py
+++ hanoi.py
@@ -1,10 +1,10 @@
 def hanoi(n, start, support, finish):
-    if (n > 1):
+    if (n == 1):
+        print("Move " + start + " to " + finish)
+    else:
         hanoi(n-1, start, finish, support)
         print("Move " + start + " to " + finish)
         hanoi(n-1, support, start, finish)
-    else:
-        print("Move " + start + " to " + finish)
 
 if __name__ == '__main__':
     hanoi(3, 'L', 'C', 'R')
